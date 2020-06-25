139780ff090cae720932a675e5c7a04d1b686a5d
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-06-25 17:33:13 +0100

    ignore this commit

---
+++ src/app/accessories/accessories-products/accessories-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accessories-products',
+  templateUrl: './accessories-products.component.html',
+  styleUrls: ['./accessories-products.component.css']
+})
+export class AccessoriesProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
