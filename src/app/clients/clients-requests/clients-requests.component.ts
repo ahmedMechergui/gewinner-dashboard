218f35ea7888c9713016358e0d314be27e1497a7
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-07-01 18:16:31 +0100

    clients-requests page done

--- src/app/clients/clients-requests/clients-requests.component.ts
+++ src/app/clients/clients-requests/clients-requests.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ListViewLoaderService} from '../../list-view-loader.service';
+import {OrdersListManagementService} from '../../orders-list-management.service';
 
 @Component({
   selector: 'app-clients-requests',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsRequestsComponent implements OnInit {
 
-  constructor() { }
+  selectedElementIndex: number;
+  ordersArray = [
+    {type: 'Quality control', client: 'Ahmed Mechergui', date : new Date('2020-7-23'), status: 'pending'},
+    {type: 'Training session', client: 'John Doe', date : new Date('2020-7-22'), status: 'pending'},
+    {type: 'Quality control', client: 'Mark Doe', date : new Date('2020-7-21'), status: 'processed'},
+    {type: 'Maintenance', client: 'Jessika Doe', date : new Date('2020-7-21'), status: 'rejected'},
+    {type: 'Quality control', client: 'Ahmed Mechergui', date : new Date('2020-7-23'), status: 'pending'},
+    {type: 'Training session', client: 'John Doe', date : new Date('2020-7-22'), status: 'pending'},
+    {type: 'Quality control', client: 'Mark Doe', date : new Date('2020-7-21'), status: 'processed'},
+    {type: 'Maintenance', client: 'Jessika Doe', date : new Date('2020-7-21'), status: 'rejected'},
+    {type: 'Quality control', client: 'Ahmed Mechergui', date : new Date('2020-7-23'), status: 'pending'},
+    {type: 'Training session', client: 'John Doe', date : new Date('2020-7-22'), status: 'pending'},
+    {type: 'Maintenance', client: 'Mark Doe', date : new Date('2020-7-21'), status: 'processed'},
+    {type: 'Training session', client: 'Jessika Doe', date : new Date('2020-7-21'), status: 'rejected'},
+    {type: 'Quality control', client: 'Ahmed Mechergui', date : new Date('2020-7-23'), status: 'pending'},
+    {type: 'Training session', client: 'John Doe', date : new Date('2020-7-22'), status: 'pending'},
+    {type: 'Quality control', client: 'Mark Doe', date : new Date('2020-7-21'), status: 'processed'},
+    {type: 'Maintenance', client: 'Jessika Doe', date : new Date('2020-7-21'), status: 'rejected'}
+  ];
+
+  constructor(
+    private listViewLoaderService: ListViewLoaderService,
+    public ordersListManagementService: OrdersListManagementService) {
+  }
 
   ngOnInit(): void {
+    this.listViewLoaderService.loadStylesheets();
+    this.listViewLoaderService.loadDataListViewScript().then();
+    this.ordersArray.reverse();
+    this.ordersListManagementService.setOrdersArray(this.ordersArray);
   }
 
 }
