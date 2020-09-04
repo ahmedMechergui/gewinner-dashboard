f6fc8724fc81fd19766763c87b6d46c69fa9e3b1
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-09-04 09:35:16 +0100

    client requests done

--- src/app/clients/clients-requests/clients-requests.component.ts
+++ src/app/clients/clients-requests/clients-requests.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ListViewLoaderService} from '../../list-view-loader.service';
 import {OrdersListManagementService} from '../../orders-list-management.service';
+import {ClientRequestsHttpService} from '../client-requests-http.service';
+import {ServiceRequest} from '../../shared/models/service-request.model';
+import {ToastService} from '../../shared/services/toast.service';
 
 @Component({
   selector: 'app-clients-requests',
@@ -9,36 +12,84 @@ import {OrdersListManagementService} from '../../orders-list-management.service'
 })
 export class ClientsRequestsComponent implements OnInit {
 
-  selectedElementIndex: number;
-  ordersArray = [
-    {type: 'Quality control', client: 'Ahmed Mechergui', date : new Date('2020-7-23'), status: 'pending'},
-    {type: 'Training session', client: 'John Doe', date : new Date('2020-7-22'), status: 'pending'},
-    {type: 'Quality control', client: 'Mark Doe', date : new Date('2020-7-21'), status: 'processed'},
-    {type: 'Maintenance', client: 'Jessika Doe', date : new Date('2020-7-21'), status: 'rejected'},
-    {type: 'Quality control', client: 'Ahmed Mechergui', date : new Date('2020-7-23'), status: 'pending'},
-    {type: 'Training session', client: 'John Doe', date : new Date('2020-7-22'), status: 'pending'},
-    {type: 'Quality control', client: 'Mark Doe', date : new Date('2020-7-21'), status: 'processed'},
-    {type: 'Maintenance', client: 'Jessika Doe', date : new Date('2020-7-21'), status: 'rejected'},
-    {type: 'Quality control', client: 'Ahmed Mechergui', date : new Date('2020-7-23'), status: 'pending'},
-    {type: 'Training session', client: 'John Doe', date : new Date('2020-7-22'), status: 'pending'},
-    {type: 'Maintenance', client: 'Mark Doe', date : new Date('2020-7-21'), status: 'processed'},
-    {type: 'Training session', client: 'Jessika Doe', date : new Date('2020-7-21'), status: 'rejected'},
-    {type: 'Quality control', client: 'Ahmed Mechergui', date : new Date('2020-7-23'), status: 'pending'},
-    {type: 'Training session', client: 'John Doe', date : new Date('2020-7-22'), status: 'pending'},
-    {type: 'Quality control', client: 'Mark Doe', date : new Date('2020-7-21'), status: 'processed'},
-    {type: 'Maintenance', client: 'Jessika Doe', date : new Date('2020-7-21'), status: 'rejected'}
+  selectedElementIndex = 0;
+  servicesArray: ServiceRequest[] = [
+    // {type: 'Quality control', client: 'Ahmed Mechergui', date: new Date('2020-7-23'), status: 'pending'},
   ];
 
   constructor(
     private listViewLoaderService: ListViewLoaderService,
-    public ordersListManagementService: OrdersListManagementService) {
+    public ordersListManagementService: OrdersListManagementService,
+    private httpRequest: ClientRequestsHttpService,
+    private toaster: ToastService) {
   }
 
   ngOnInit(): void {
     this.listViewLoaderService.loadStylesheets();
     this.listViewLoaderService.loadDataListViewScript().then();
-    this.ordersArray.reverse();
-    this.ordersListManagementService.setOrdersArray(this.ordersArray);
+    this.servicesArray.reverse();
+    this.ordersListManagementService.setOrdersArray(this.servicesArray);
+    this.servicesArray = this.httpRequest.serviceRequestsArray;
+    this.httpRequest.getAllServiceRequests();
+  }
+
+  getSelectedElement() {
+    return this.servicesArray[this.selectedElementIndex];
+  }
+
+  /*==================
+    Http requests
+    ==================*/
+
+  changeStatus(status: string) {
+    switch (this.getSelectedElement().type) {
+      case 'Training session' :
+        this.changeStatusTrainingSession(status);
+        break;
+      case 'Maintenance' :
+        this.changeMaintenanceStatus(status === 'validated' ? 'fixed' : 'rejected');
+        break;
+      case 'Quality control' :
+        this.changeQualityControlStatus(status);
+        break;
+    }
+  }
+
+  changeStatusTrainingSession(status: string) {
+    this.httpRequest.changeStatusTrainingSession(this.getSelectedElement().id, status).subscribe(() => {
+      this.getSelectedElement().status = status;
+      this.toaster.success('Service request updated', 'Done');
+    }, () => {
+      this.toaster.error('Unable to perform updates', 'Error :');
+    });
+  }
+
+  changeMaintenanceStatus(status: string) {
+    this.httpRequest.changeStatusMaintenance(this.getSelectedElement().id, status).subscribe(() => {
+      this.getSelectedElement().status = status;
+      this.toaster.success('Service request updated', 'Done');
+    }, () => {
+      this.toaster.error('Unable to perform updates', 'Error :');
+    });
+  }
+
+  changeQualityControlStatus(status: string) {
+    this.httpRequest.changeQualityControlStatus(this.getSelectedElement().id, status).subscribe(() => {
+      this.toaster.success('Service request updated', 'Done');
+      this.getSelectedElement().status = status;
+    }, () => {
+      this.toaster.error('Unable to perform updates', 'Error :');
+    });
+  }
+
+  deleteServiceRequest() {
+    this.httpRequest.deleteServiceRequest(this.getSelectedElement().id,
+      this.getSelectedElement().type).subscribe(() => {
+      this.servicesArray.splice(this.selectedElementIndex, 1);
+      this.toaster.success('Service request deleted', 'Done');
+    }, () => {
+      this.toaster.error('Unable to delete service request', 'Error :');
+    });
   }
 
 }
