5414208cdaf114338e204172e0b7eb2ade680888
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-10-14 12:21:26 +0100

    a

--- src/app/shared/services/host-url.service.ts
+++ src/app/shared/services/host-url.service.ts
@@ -4,8 +4,8 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class HostUrlService {
-  url = 'http://127.0.0.1:3000';
-  // url = 'https://gewinner-api.herokuapp.com';
+  // url = 'http://127.0.0.1:3000';
+  url = 'https://gewinner-api.herokuapp.com';
 
   constructor() {
   }
