a1015442baf55a224eba4b68ac9973b0eb741930
ahmedMechergui <ahmed.michrgui@hotmail.fr>
2020-12-27 14:43:42 +0100

    done!

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 
@@ -12,5 +14,26 @@ app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/dashboard/index.html'));
 });
 
+const PORT = 3500;
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3500);
+// app.listen(process.env.PORT || 3500);
+
+/* ===============================
+ # SSL configuration
+   ===============================*/
+
+const sslOptions = {
+
+  key: fs.readFileSync('ssl-certif/private.key'),
+  cert: fs.readFileSync('ssl-certif/certificate.crt'),
+
+  ca: [
+    fs.readFileSync('ssl-certif/ca_bundle.crt')
+  ]
+};
+
+
+const httpsServer = https.createServer(sslOptions, app);
+
+httpsServer.listen(+PORT);
